237e68e9cee61503c86e2da6f3afad810f0bb539
JeanetteLeon <jeanette.leon@usach.cl>
2025-07-13 15:18:14 -0400

    Modificar el config para que guarde los datos procesado de testeo y testeo

--- src/config.py
+++ src/config.py
@@ -19,11 +19,19 @@ EPS = 1e-7
 # --- Dataset (ruta por defecto para entrenamiento) ---
 DATA_URL_JAN = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2020-01.parquet"
 
+
+# --- Ruta del datos procesados para entramiento ---
+TRAIN_PATH = "C:\\Users\\jeane_bkpplgv\\OneDrive\\Escritorio\\UDD\\5° Trimestre\\Desarrollo de Proyectos y Productos de Datos\\tarea_1\\data\\processed\\train\\train_data"
+
+
+# --- Ruta del datos procesados para testeo ---
+TRAIN_PATH = "C:\\Users\\jeane_bkpplgv\\OneDrive\\Escritorio\\UDD\\5° Trimestre\\Desarrollo de Proyectos y Productos de Datos\\tarea_1\\data\\processed\\test\\test_data"
+
 # --- Ruta del modelo entrenado ---
-MODEL_PATH = "C:\\Users\\jeane_bkpplgv\\OneDrive\\Escritorio\\UDD\\5° Trimestre\\Desarrollo de Proyectos y Productos de Datos\\tarea_1\\model_outputs\\model.joblib"
+MODEL_PATH = "C:\\Users\\jeane_bkpplgv\\OneDrive\\Escritorio\\UDD\\5° Trimestre\\Desarrollo de Proyectos y Productos de Datos\\tarea_1\\models\\model.joblib"
 
 # --- Carpeta externa donde están los datasets mensuales ---
-PARQUET_BASE_PATH = "C:\\Users\\jeane_bkpplgv\\OneDrive\\Escritorio\\UDD\\5° Trimestre\\Desarrollo de Proyectos y Productos de Datos\\Tarea_1\\dataset_months"
+PARQUET_BASE_PATH = "C:\\Users\\jeane_bkpplgv\\OneDrive\\Escritorio\\UDD\\5° Trimestre\\Desarrollo de Proyectos y Productos de Datos\\Tarea_1\\data\\raw\\dataset_months"
 
 # --- Meses para evaluación del modelo ---
 MESES_EVALUACION = ['2020-02', '2020-03', '2020-04']
