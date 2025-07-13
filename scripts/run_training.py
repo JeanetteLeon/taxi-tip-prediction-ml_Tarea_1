98f8576c5886288fd7f82af0fe93796282b2ad06
JeanetteLeon <jeanette.leon@usach.cl>
2025-07-12 21:41:27 -0400

    Modificar run_training.py

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -1,9 +1,14 @@
 # scripts/run_training.py
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.data.dataset import load_dataset, clean_data
 from src.features.build_features import build_features
 from src.modeling.train import train_model, save_model
 from src.config import DATA_URL_JAN
+
 
 def main():
     print("Cargando datos...")
