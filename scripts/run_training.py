1b3acf40d699acb36fff0ba239fe1eb804264ba9
JeanetteLeon <jeanette.leon@usach.cl>
2025-07-12 21:46:22 -0400

    Modificar run_training.py

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -2,7 +2,8 @@
 
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 from src.data.dataset import load_dataset, clean_data
 from src.features.build_features import build_features
